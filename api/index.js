c6bfe5b97241dcc5d09e796978d39ba972f5971b
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-07-05 10:16:41 +0530

    created server and database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+// connect to database
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("connected to database"))
+  .catch((err) => console.log("connection failed"));
 const app = express();
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
