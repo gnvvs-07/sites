d65dbf7ba14e19353edcbad70d460cf8c94f32e1
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-07-05 10:34:23 +0530

    test api routing completed

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "../api/routes/user.route.js";
 dotenv.config();
+
 // connect to database
 mongoose
   .connect(process.env.MONGO)
@@ -11,3 +13,5 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+// api routing
+app.use("/api/user", userRouter);
