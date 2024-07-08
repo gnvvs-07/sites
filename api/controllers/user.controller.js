6295c127d01ddf20fdc8d66ef9c4e4300939a145
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-07-08 15:24:32 +0530

    user deletion -100%

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -44,6 +44,7 @@ export const deleteUser = async (req, res, next) => {
     return next(errorHandler(401, "You can only delete your account"));
   try {
     await User.findByIdAndDelete(req.params.id);
+    res.clearCookie("access_token");
     res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
     next(error);
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -39,6 +39,19 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+    // delete user
+    deleteUserStart: (state) => {
+      state.loading = true;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
@@ -49,6 +62,9 @@ export const {
   updateUserFailure,
   updateUserStart,
   updateUserSuccess,
+  deleteUserFailure,
+  deleteUserStart,
+  deleteUserSuccess
 } = userSlice.actions;
 
 export default userSlice.reducer;
