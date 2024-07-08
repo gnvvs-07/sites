153041ba3305a22cdff34c5181626dd16185bd3d
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-07-08 15:13:43 +0530

    user deletion api -100%

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -8,5 +8,5 @@ const router = express.Router();
 router.get("/test", test);
 // update api
 router.post("/update/:id", verifyToken, updateUser);
-
+router.delete("/delete/:id",verifyToken,deleteUser);
 export default router;
