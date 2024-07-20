0b9b469b568da62ad5d0fe17491ef20f7c55ff2a
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-07-20 10:39:31 +0530

    contact landlord functionality completed

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -12,15 +12,16 @@ import {
   FaParking,
   FaShare,
 } from "react-icons/fa";
-
-// https://sabe.io/blog/javascript-format-numbers-commas#:~:text=The%20best%20way%20to%20format,format%20the%20number%20with%20commas.
-
+import { useSelector } from "react-redux";
+import Contact from "../components/Contact";
 export default function Listing() {
   SwiperCore.use([Navigation]);
   const [listing, setListing] = useState(null);
+  const [contact, setContact] = useState(false);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [copied, setCopied] = useState(false);
+  const { currentUser } = useSelector((state) => state.user);
   const params = useParams();
   useEffect(() => {
     const fetchListing = async () => {
@@ -131,6 +132,15 @@ export default function Listing() {
                 {listing.furnished ? "Furnished" : "Unfurnished"}
               </li>
             </ul>
+            {currentUser && listing.userRef !== currentUser._id && !contact && (
+              <button
+                onClick={() => setContact(true)}
+                className="bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3"
+              >
+                Contact landlord
+              </button>
+            )}
+            {contact && <Contact listing={listing} />}
           </div>
         </div>
       )}
